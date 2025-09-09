ff8ac363a13bbfcf59c1a15e42430d164829c429
sbt80171 <sbt80171@gmail.com>
2025-09-09 16:11:33 +0900

    :art: change UI

--- diff.py
+++ diff.py
@@ -10,64 +10,57 @@ def main():
         layout='wide'
     )
     st.title('Diff')
-    st.write('テキスト差分表示アプリ')
+    st.markdown('テキスト差分表示アプリ')
 
     with st.form(key='content'):
         col_from, col_to = st.columns(2)
 
-        def text_area(key: str):
+        def text_area(label: str, key: str):
             initial_height = 300
             return st.text_area(
-                label='_',
+                label=label,
                 height=initial_height,
                 placeholder='テキストを入力してください',
-                label_visibility='collapsed',
                 key=key
             )
 
         with col_from:
-            st.write(':material/Check: もとのテキスト')
-            text_area(key='content_from')
+            text_area(
+                label=':material/Check: もとのテキスト',
+                key='content_from'
+            )
         with col_to:
-            st.write(':material/Check: くらべるテキスト')
-            text_area(key='content_to')
+            text_area(
+                label=':material/Check: くらべるテキスト',
+                key='content_to'
+            )
 
-        col1, col2, col3 = st.columns(3, vertical_alignment='bottom')
-        with col1:
-            st.write(':material/Check: テキスト差分の折り返しカラム幅')
-            wrapcolumn = st.number_input(
-                label='_',
-                min_value=0,
-                step=1,
-                label_visibility='collapsed'
-            )
-        with col2:
-            st.caption('0 で折り返しなし')
-        with col3:
-            st.write(':material/Check: 変更点前後のみ表示')
-            is_context = st.toggle(
-                label='_',
-                label_visibility='collapsed'
-            )
+        wrapcolumn = st.number_input(
+            label=':material/Check: テキスト差分の折り返しカラム幅',
+            min_value=0,
+            step=1,
+            help='0 で折り返しなし'
+        )
+        is_context = st.toggle(label='変更点前後のみ表示')
 
         st.form_submit_button(
-            label='差分を表示',
-            type='primary'
+            label='差分を表示'
         )
 
     if st.session_state['content_from'] or st.session_state['content_to']:
-        st.write(':sparkles: テキスト差分')
+        with st.container(border=True):
+            st.write(':sparkles: テキスト差分')
 
-        diff = HtmlDiff(wrapcolumn=wrapcolumn)
-        diff._styles += 'td {background-color: white; color: black;}' # type: ignore
+            diff = HtmlDiff(wrapcolumn=wrapcolumn)
+            diff._styles += 'td {background-color: white; color: black;}' # type: ignore
 
-        html_diff = diff.make_file(
-            fromlines=st.session_state['content_from'].splitlines(),
-            tolines=st.session_state['content_to'].splitlines(),
-            context=is_context
-        )
+            html_diff = diff.make_file(
+                fromlines=st.session_state['content_from'].splitlines(),
+                tolines=st.session_state['content_to'].splitlines(),
+                context=is_context
+            )
 
-        st.html(body=html_diff)
+            st.html(body=html_diff)
 
     st.markdown("""
     * ブラウザ更新でリセットできます
